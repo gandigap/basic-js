6dad5cbd5e0af95a563185fb142ae21f544129dd
Ihar Berasneu <igor.simatic@gmail.com>
2020-09-27 18:26:39 +0300

    feat:add decision task transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,32 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error('');
+  }
+
+  let newArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === '--discard-next') {
+      i++;
+      continue;
+    } else if (arr[i] === '--discard-prev' && arr[i - 2] !== '--discard-next') {
+      newArr.pop();
+    } else if (arr[i] === '--double-next') {
+      if (arr[i + 1] !== undefined) newArr.push(arr[i + 1]);
+      i++;
+    } else if (arr[i] === '--double-prev' && arr[i - 2] !== '--discard-next') {
+      if (arr[i - 1] !== undefined) newArr.push(arr[i - 1]);
+      i++;
+    }
+
+    if (arr[i] !== '--discard-next' &&
+      arr[i] !== '--discard-prev' &&
+      arr[i] !== '--double-next' &&
+      arr[i] !== '--double-prev' &&
+      arr[i] !== undefined) newArr.push(arr[i]);
+  }
+
+  return newArr;
 };
