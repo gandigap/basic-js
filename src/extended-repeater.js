97764d047008af38105ecbbdff94573ec08aa1e4
Ihar Berasneu <igor.simatic@gmail.com>
2020-10-06 20:49:24 +0300

    feat:add decision tasks hanoi tower and extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,33 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  str = (typeof str != 'string') ? String(str) : str;
+  let separator = (options.separator === undefined) ? '+' : options.separator;
+  let repeatTimes = (options.repeatTimes === undefined) ? 1 : options.repeatTimes;
+  let addition = (options.addition === undefined) ? '' : String(options.addition);
+  let additionSeparator = (options.additionSeparator === undefined) ? '|' : options.additionSeparator;
+  let additionRepeatTimes = (options.additionRepeatTimes === undefined) ? 1 : options.additionRepeatTimes;
+
+  let resultString = '';
+  let additionResultString = '';
+
+  let arrayMainString = [];
+  let arrayAdditionString = [];
+
+  for (let i = 0; i < additionRepeatTimes; i++) {
+    arrayAdditionString.push(addition);
+  }
+
+  additionResultString = str + arrayAdditionString.join(additionSeparator);
+
+  for (let i = 0; i < repeatTimes; i++) {
+    arrayMainString.push(additionResultString);
+  }
+
+  resultString = arrayMainString.join(separator)
+
+
+  // STRINGPLUS00PLUS00PLUS00 ** STRINGPLUS00PLUS00PLUS00 ** STRINGPLUS00PLUS00PLUS00
+  // STRINGPLUS00PLUS00PLUS ** STRINGPLUS00PLUS00PLUS ** STRINGPLUS00PLUS00PLUS
+  return resultString;
 };
-  
--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,9 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+  const SEC_PER_OUR = 60 * 60;
+  let turns = 2 ** disksNumber - 1;
+  let seconds = Math.floor(SEC_PER_OUR / turnsSpeed * turns);
+  return { turns, seconds };
   // remove line with error and write your code here
 };
