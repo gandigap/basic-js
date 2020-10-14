c4a57a5cdb9a95f31ecbd6ce255f555ee189403a
Ihar Berasneu <igor.simatic@gmail.com>
2020-10-14 21:24:09 +0300

    feat:add decision for task simple chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,35 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
+
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    (value === undefined) ? this.chain.push(`'(  )'`) : this.chain.push(`( ${value} )`);
+    return this;
   },
+
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (position === undefined || position <= 0 || position > this.chain.length || typeof position !== 'number') {
+      this.chain = [];
+      throw new Error();
+    } else {
+      this.chain.splice(position - 1, 1);
+      return this;
+    }
   },
+
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
+
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let completeChain = this.chain;
+    this.chain = [];
+    return completeChain.join('~~');
   }
 };
 
